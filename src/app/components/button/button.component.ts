2133e5fc87424766a10f858f12679807efa2090f
Robby <ta7382@gmail.com>
2018-12-07 10:11:21 +0800

    Update componets router link

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
+import { ClrLoadingState } from '@clr/angular';
+
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+
+  validateBtnState: ClrLoadingState = ClrLoadingState.DEFAULT;
+  validateBtnStateError: ClrLoadingState = ClrLoadingState.DEFAULT;
+  submitBtnState: ClrLoadingState = ClrLoadingState.DEFAULT;
 
   constructor(
     private route: ActivatedRoute,
@@ -19,6 +25,27 @@ export class ButtonComponent implements OnInit {
       .subscribe((data: { title: string }) => {
         this.titleService.setTitle(data.title);
       });
+  }
+
+  validateDemo() {
+    this.validateBtnState = ClrLoadingState.LOADING;
+    setTimeout(() => {
+      this.validateBtnState = ClrLoadingState.SUCCESS;
+    }, 2000);
+  }
+
+  validateErrorDemo() {
+    this.validateBtnStateError = ClrLoadingState.LOADING;
+    setTimeout(() => {
+      this.validateBtnStateError = ClrLoadingState.ERROR;
+    }, 2000);
+  }
+
+  submitDemo() {
+    this.submitBtnState = ClrLoadingState.LOADING;
+    setTimeout(() => {
+      this.submitBtnState = ClrLoadingState.DEFAULT;
+    }, 2000);
   }
 
 }
